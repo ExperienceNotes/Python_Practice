a0f719435150e487ec638f5d5b31cb1af288ff92
Wei <ar4031cejob@gmail.com>
2021-09-24 14:14:45 +0800

    git add Pandas_F1_score.py

---
+++ Python_Pandas/Pandas_F1_score.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.sparse import data
+from sklearn.metrics import precision_score,recall_score,fbeta_score
+y_pred = np.random.randint(2,size = 100)
+y_true = np.random.randint(2,size = 100)
+
+def custom_fbeta_socre(y_pred,y_true,beta = 1):
+    precision = precision_score(y_true,y_pred)
+    recall = recall_score(y_true,y_pred)
+    
+    fbeta = (1+(beta)**2) * (precision*recall)/(beta**2*precision + recall)
+    return fbeta
+print(custom_fbeta_socre(y_pred,y_true,beta=2))
+print(fbeta_score(y_true, y_pred, beta=2))
